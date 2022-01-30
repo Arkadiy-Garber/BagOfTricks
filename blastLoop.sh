44abc8c87c81181e51dd9d44aa1bc9af9f1c93fe
Arkadiy-Garber <agarber4@asu.edu>
2022-01-30 00:20:52 -0500

    Delete blastLoop.sh

--- blastLoop.sh
+++
@@ -1,3 +0,0 @@
-for i in *faa; do
-diamond blastp --query $i --db /scratch/1/arkadiy/databases/nr/nr.faa --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore stitle --out ${i%.*}.nr.blast --max-target-seqs 1 --evalue 1E-6
-done
